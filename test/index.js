c3831fca7c3950f5babc85b6859f65a42f0e7066
Qubeo <32247672+Qubeo@users.noreply.github.com>
2018-12-08 03:37:51 +0100

    #05

--- test/index.js
+++ test/index.js
@@ -1,22 +1,51 @@
 // This test file uses the tape testing framework.
 // To learn more, go here: https://github.com/substack/tape
 const test = require('tape');
+const tapSpec = require('tap-spec');
+//const hash = require('hash.js');
+var shajs = require('sha.js');
 const Container = require('@holochain/holochain-nodejs');
+const bs58 = require('bs58');
+
+
+
+test.createStream()
+  .pipe(tapSpec())
+  .pipe(process.stdout);
 
 // instantiate an app from the DNA JSON bundle
-const app = Container.loadAndInstantiate("dist/bundle.json")
+const app = Container.loadAndInstantiate("dist/bundle.json");
 
 // activate the new instance
-app.start()
+app.start();
 
-test('description of example test', (t) => {
+test('Calls the who_am_I function, expects result to be proper handle', (t) => {
   // Make a call to a Zome function
   // indicating the capability and function, and passing it an input
-  // const result = app.call("zome-name", "capability-name", "function-name", {})
+ 
+  const result = app.call("cointoss", "main", "who_am_I", {});
+  console.log("Result: ");
+  console.log(result);
+  // TODO: What bytes does the result exactly consist of?
+  // Check for equality of the actual and expected results:
 
-  // check for equality of the actual and expected results
-  // t.equal(result, "expected result!")
+  // Decode from bs58 to hex, slice the leading 2 bytes, encode back to bs58
+  // Q: Why the leading 2 bytes? No idea.
+  var recodedResult = bs58.encode(Buffer.from(bs58.decode(result)).slice(2));
+  
+  console.log("Result slice(2)-ed & recoded: ");
+  console.log(recodedResult);
+  
+  var hashedPrdel = shajs('sha256').update('prdel').digest();
 
-  // ends this test
-  t.end()
+  // TODO: Vypadá to, že když dám console log s úvodním stringem, ten recoded result a hashedPrdel jsou stejné, akorát recodedResult má dva bajty navíc.
+  // JAKTO?? 34b. Je to už ten result?
+
+  const b58Prdel = bs58.encode(Buffer.from(hashedPrdel));  
+  console.log("SHA256-ed & bs58-ed prdel: ");
+  console.log(hashedPrdel);
+
+  t.equal(recodedResult, b58Prdel);
+
+  t.end();
 })
--- zomes/cointoss/code/src/lib.rs
+++ zomes/cointoss/code/src/lib.rs
@@ -10,9 +10,14 @@ extern crate serde_derive;
 extern crate holochain_core_types_derive;
 #[macro_use]
 extern crate serde_json;
+#[macro_use]
+extern crate log;
+
+extern crate multihash;
 
 use std::io;
 use rand::Rng;
+use multihash::{encode, decode, Hash};
 
 // see https://developer.holochain.org/api/0.0.2/hdk/ for info on using the hdk library
 
@@ -30,49 +35,21 @@ use hdk::holochain_core_types::{
 // mod toss;
 
 #[derive(Serialize, Deserialize, Debug, DefaultJson)]
-pub struct toss_schema {
+pub struct TossSchema {
     pub initiator: Address,          // Q: Or should this be a JSON? Or json_serde? .into()?
-    pub initiatorSeedHash: HashString, 
+    pub initiator_seed_hash: HashString, 
     pub responder: Address,         // Q: Or? Shouldn't be hdk's hash::HashString, cas::content::Address or something like that?
-    pub responderSeedHash: HashString,
+    pub responder_seed_hash: HashString,
     pub call: bool                  // Q: What the heck is this?
-    // pub required: ["initiator", "initiatorSeedHash", "responder", "responderSeedHash"]; // Q: How to initialize the field?
-    
-    /*
-        "title": "Toss Schema",
-        "type": "object",
-        "properties": {
-            "initiator": {
-                "type": "string"
-            },
-            "initiatorSeedHash": {
-                "type": "string"
-            },
-            "responder": {
-                "type": "string"
-            },
-            "responderSeedHash": {
-                "type": "string"
-            },
-            "call": {
-                "type": "boolean"
-            }
-        },
-        "required": [
-            "initiator",
-            "initiatorSeedHash",
-            "responder",
-            "responderSeedHash"
-        ]
-    */
+    // pub required: ["initiator", "initiator_seed_hash", "responder", "responder_seed_hash"]; // Q: How to initialize the field?
 }
 
 #[derive(Serialize, Deserialize, Debug, DefaultJson)]
-pub struct toss_result_schema {
-    pub toss: toss_schema, //Q: Or &toss_schema? Or string, as in the original JS version?
-	pub result: String,
-    pub timeStamp: String
-    // pub required:  ["toss","result","timeStamp"] // Q: Validation rules?
+pub struct TossResultSchema {
+    pub toss: TossSchema, //Q: Or &TossSchema? Or string, as in the original JS version?
+	pub result: String,    // Q: What format?
+    pub time_stamp: String
+    // pub required:  ["toss","result","time_stamp"] // Q: Validation rules?
 }
 
 // -------------------------------------- TOSS FUNCTIONS ------------------------------------------
@@ -80,12 +57,15 @@ pub struct toss_result_schema {
 
 pub fn handle_who_am_I() -> JsonString {
     
-    return json!(HashString::new()).into();
-    
+    // TODO: Not fully implemented AGENT_ADDRESS in the current HDK release yet.
+    // Temporary workaround idea: use a random hash? Or hash agent key? Where do I get it?
+ 
+    // TODO: VERY temporary - just returning a hard-coded HashString now.
+    return set_my_key().into();
 }
 
 
-pub fn handle_set_handle(handle: HashString) -> JsonString {
+pub fn handle_set_handle(_handle: HashString) -> JsonString {
     return json!(HashString::new()).into();
 }
 
@@ -96,7 +76,7 @@ pub fn handle_get_handles() -> JsonString {
 }
 
 // returns the handle of an agent by looking it up on the user's DHT entry, the last handle will be the current one?
-pub fn handle_get_handle(handle: HashString) -> JsonString {
+pub fn handle_get_handle(_handle: HashString) -> JsonString {
     return HashString::new().into();
 }
 
@@ -105,7 +85,7 @@ pub fn handle_get_my_handle() -> JsonString {
 }
 
 // gets the AgentID (userAddress) based on handle
-pub fn handle_get_agent(handle: HashString) -> JsonString {
+pub fn handle_get_agent(_handle: HashString) -> JsonString {
     return Address::new().into();
 }
 
@@ -117,34 +97,37 @@ pub fn handle_get_toss_history() -> JsonString {
 / pub fn handle_request_toss()
 / Request the toss - initiating the game by doing the first seed commit and sending the request to the agent through gossip (?)
 */
-pub fn handle_request_toss(toss: toss_schema) -> JsonString {
+pub fn handle_request_toss(_toss: TossSchema) -> JsonString {
 
     let seed = rand::thread_rng().gen_range(0, 10);
     println!("Generated seed: {}", seed);
-    handle_commit_seed(seed);
+    commit_seed(seed);
     
     return HashString::new().into();
 }
 
+fn handle_confirm_toss(_toss: TossSchema) -> JsonString {
+    return HashString::new().into();
+}
+
+fn set_my_key() -> HashString {
+
+    //let mut hasher = DefaultHasher::new();
+
+    let my_key = HashString::encode_from_str("prdel", Hash::SHA2256);
+    hdk::debug("my_key: ");
+    hdk::debug(&my_key);
+
+    return my_key; 
+}
+
 // Commit functions - should they be a part of the zome? Or private? Or both?
-fn handle_confirm_seed() -> JsonString {
-        return HashString::new().into();
-}
-
-fn handle_commit_toss() -> JsonString {
-    return HashString::new().into();
-}
-
-fn handle_confirm_toss(toss: toss_schema) -> JsonString {
-    return HashString::new().into();
-}
-
 
 /*
 / fn commit_seed()
 / return: ???
 */
-fn handle_commit_seed(seed: i32) -> JsonString {
+fn commit_seed(_seed: i32) -> JsonString {
 
     // Validate if 9 <= seed >= 0 
     // Generate salt
@@ -155,11 +138,17 @@ fn handle_commit_seed(seed: i32) -> JsonString {
     return HashString::new().into();
 }
 
-fn generate_salt() -> JsonString {
+fn confirm_seed() -> JsonString {
+        return HashString::new().into();
+}
+
+fn commit_toss() -> JsonString {
     return HashString::new().into();
 }
 
-
+fn generate_salt() -> JsonString {
+    return HashString::new().into();
+}
 
 
 define_zome! {
@@ -194,7 +183,7 @@ define_zome! {
             name: "toss",
             description: "",
             sharing: Sharing::Public,
-            native_type: toss_schema, // Q: Or? Json? JsonString?
+            native_type: TossSchema, // Q: Or? Json? JsonString?
             validation_package: || { 
                 hdk::ValidationPackageDefinition::Entry
             },
@@ -206,7 +195,7 @@ define_zome! {
             name: "toss_result",
             description: "",
             sharing: Sharing::Public,
-            native_type: toss_result_schema, // Q: Or?
+            native_type: TossResultSchema, // Q: Or?
             validation_package: || { 
                 hdk::ValidationPackageDefinition::Entry
             },
@@ -241,7 +230,15 @@ define_zome! {
 
     ]
 
-    genesis: || { Ok(()) }
+    genesis: || {
+        
+         // TODO workaround around not-yet-implemented hdk::api::AGENT_ADDRESS
+         // Commit a tomporarily created agent hash to my chain and return the entry address?
+            {
+                // set_my_key();        
+                Ok(())
+            }
+         }
 
     functions: {
         main (Public) {
@@ -252,7 +249,7 @@ define_zome! {
 			}
     		set_handle: {
 				inputs: |handle: HashString|,
-				outputs: |result: JsonString|,
+				outputs: |result: JsonString|,      // Q: How does this syntax work? Closure arguments without follow up function body? :o
 				handler: handle_set_handle
 			}
             get_handles: {
@@ -276,12 +273,12 @@ define_zome! {
 				handler: handle_get_agent
 			}
             request_toss: {
-				inputs: |request: toss_schema |,
+				inputs: |request: TossSchema |,
 				outputs: |result: JsonString|,
 				handler: handle_request_toss
 			}
             confirm_toss: {
-				inputs: |toss: toss_schema|,
+				inputs: |toss: TossSchema|,
 				outputs: |result: JsonString|,
 				handler: handle_confirm_toss
 			}
